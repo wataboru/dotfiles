24445de47cb2c71c58c66b537fe76524ef5fb230
Wataru Sato <watarusatoh60@gmail.com>
2022-04-12 14:16:18 +0900

    Add gitignore and modify alias

--- .config.fish
+++ .config.fish
@@ -1,15 +1,7 @@
-### GOPATH
-set GOPATH /Users/work/workspace/go
-set GOROOT /Users/work/sdk/go1.14.3
-
 ### PATH
 set PATH /usr/local/bin /usr/sbin $PATH
 set PATH /usr/local/opt/libxml2/bin $PATH
 set PATH /usr/local/opt/libxslt/bin $PATH
-set PATH /Applications/MAMP/Library/bin $PATH
-set PATH $GOROOT/bin $PATH
-set PATH /usr/local/flutter/bin $PATH
-set PATH $GOPATH/bin $PATH
 
 ### disable goenv
 set GOENV_DISABLE_GOPATH 1
@@ -21,7 +13,7 @@ alias ..2="cd ../.."
 alias ..3="cd ../../.."
 alias c-="cd -"
 alias c~="cd ~"
-alias _="sudo"
+alias sd="sudo"
 alias pwdy="pwd | pbcopy"
 
 # github
